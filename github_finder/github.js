584964ed31f633c543ee97cd32136520a6e2bf83
josh-everett01 <59103702+josh-everett01@users.noreply.github.com>
2020-07-23 19:17:07 -0500

    Added JS and bootstrap styling to display users 5 most recent repos

--- github_finder/github.js
+++ github_finder/github.js
@@ -2,15 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = '9780de65aa0bb0cb7c68';
     this.client_secret = '2d00cf0ab40a516b98e9ed7c9f381390570f2fc5';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
