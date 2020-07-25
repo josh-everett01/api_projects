65c823ac177986dde5a786eac2e670fed6b7d7c9
josh-everett01 <59103702+josh-everett01@users.noreply.github.com>
2020-07-25 18:41:34 -0500

    Finished adding JS functionality. Used alternate API since API in couse isn't active anymore. This was a challenge, but overall was fun.

--- weatherjs/app.js
+++ weatherjs/app.js
@@ -1,15 +1,41 @@
+// Init Storage
+const storage = new Storage();
+// Get Stored Location Data
+const weatherLocation = storage.getLocationData();
+
 // Init Weather Object
-const weather = new Weather('burleson', 'texas');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+// Init UI
+const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather());
 
-// weather.changeLocation('longmont', 'colorado');
+// Change Location Event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  // Change Location
+  weather.changeLocation(city, state);
+
+  // Set Location in LS
+  storage.setLocationData(city, state);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
+
+
 
 function getWeather() {
   weather.getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
 }
